f3bf2f6d1904186d54bd32f8b3e33534c51920dc
Kahu-Teva <tevaa.kahu@gmail.com>
2021-04-07 16:48:21 -1000

    Row of one detailMovie added.

--- src/components/row.js
+++ src/components/row.js
@@ -1,56 +1,89 @@
 import { React, useState, useEffect } from "react";
+import RowDetail from "./rowDetail.js";
 import "./../styles/Row.css"
 
 function Row({title}){
     const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [movies, setItems] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [movies, setItems] = useState([]);
+    const [isDetailLoad, setIsLoadedDetail] = useState(false);
+    const [detail, setDetail] = useState([]);
 
-  // Fetching data
-  console.log(`"Fetching data from ${process.env.REACT_APP_SERVER_API}...`);
-  useEffect(() => {
-    fetch(process.env.REACT_APP_SERVER_API + "/movies")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log("Result : ", result);
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Remarque : il faut gérer les erreurs ici plutôt que dans
-        // un bloc catch() afin que nous n’avalions pas les exceptions
-        // dues à de véritables bugs dans les composants.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-  console.log("Fetching movies OK !");
+    // Fetching data
+    console.log(`"Fetching data from ${process.env.REACT_APP_SERVER_API}...`);
+    useEffect(() => {
+        fetch(process.env.REACT_APP_SERVER_API + "/movies")
+        .then(res => res.json())
+        .then(
+            (result) => {
+            console.log("Result : ", result);
+            setIsLoaded(true);
+            setItems(result);
+            },
+            // Remarque : il faut gérer les erreurs ici plutôt que dans
+            // un bloc catch() afin que nous n’avalions pas les exceptions
+            // dues à de véritables bugs dans les composants.
+            (error) => {
+            setIsLoaded(true);
+            setError(error);
+            }
+        )
+    },
+    [])
 
-  if (error) {
-    return <div>Erreur : {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Chargement...</div>;
-  } else {
-    return (
-        <div className="row">
-            <h2 className="row__title">{title}</h2>
-            <div className="row__posters">
-                {
-                    movies.map(movie => (
-                        <img
-                            key={movie._id}
+    console.log("Fetching movies OK !");
+    
+    function handlePoster(currentMovie){
+        setDetail(currentMovie);
+        setIsLoadedDetail(true);
+    }
+
+    if(error) {
+        return <div>Erreur : {error.message}</div>;
+    }else if(!isLoaded) {
+        return <div>Chargement...</div>;
+    }else if(!isDetailLoad){
+        return (
+            <div className="row">
+                <h2 className="row__title">{title}</h2>
+                <div className="row__posters">
+                    {
+                        movies.map(currentMovie => (
+                            <img
+                            key={currentMovie._id}
                             className="row__poster"
-                            src={movie.posterLink}
-                            alt={movie.title}
-                        />
-                    ))
-                }
+                            src={currentMovie.posterLink}
+                            alt={currentMovie.title}
+                            onClick={() => handlePoster(currentMovie)}
+                            />
+                            ))
+                        }
+                </div>
             </div>
-        </div>
-    );
-  }
-}
+        );
+    }else{
+        return (
+            <div>
 
+                <div className="row">
+                    <h2 className="row__title">{title}</h2>
+                    <div className="row__posters">
+                        {
+                            movies.map(currentMovie => (
+                                <img
+                                key={currentMovie._id}
+                                className="row__poster"
+                                src={currentMovie.posterLink}
+                                alt={currentMovie.title}
+                                onClick={() => handlePoster(currentMovie)}
+                                />
+                                ))
+                            }
+                    </div>
+                </div>
+                <RowDetail info={detail}/>
+            </div>
+        );
+    }
+}
 export default Row
---
+++ src/components/rowDetail.js
@@ -0,0 +1,38 @@
+import "./../styles/RowDetail.css"
+
+function RowDetail({info}){
+    const trailerLink = info.trailerLink + "/embed/";
+    return(
+        <div className="row__detail">
+            <div className="title">{info.title}</div>
+            <iframe className="ytPlayer" title={info.title} key={info._id} src={trailerLink} />
+        </div>
+    );
+}
+export default RowDetail;
+
+/* import "./../styles/RowDetail.css"
+
+import YouTube from "react-youtube";
+var getYouTubeID = require("get-youtube-id");
+
+function RowDetail({info}){
+    const id = getYouTubeID(info.trailerLink);
+    const opts = {
+        height: "390",
+        width: "640",
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1
+        }
+    }
+    return(
+        <div className="row__detail">
+            <div className="title">{info.title}</div>
+
+            <YouTube videoId={id} opts={opts} onReady={this._onReady} />
+            { <iframe className="ytPlayer" title={info.title} key={info._id} src={trailerLink}  /> 
+            </div>
+            );
+        }
+        export default RowDetail; */
