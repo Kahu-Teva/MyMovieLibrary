8b9369824bf11ee274f3ae250508e6d6bc650661
Kahu-Teva <tevaa.kahu@gmail.com>
2021-04-08 00:17:28 -1000

    Clean App.js and db.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,9 @@
-
-import './../styles/App.css';
-
+import React from "react";
+import Navbar    from "./navbar.js"
 import Banner from "./banner.js"
 import Row    from "./row.js"
-import Navbar    from "./navbar.js"
-
-// function App() {
-//   return(
-//     <div className="app">
-//       <Navbar />
-//       <Banner/>
-//       <Row title="Les plus gros succès sur MML"/>
-      
-//       {/* <Row title="Tendance actuelles"/>
-//       <Row title="Série palpitantes"/>
-//       <Row title="Documentaire"/>
-//       <Row title="Série d'actions"/> */}
-
-//     </div>
-//   )
-// }
-
-// export default App;
-
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './../styles/App.css';
 
 export default function App() {
   return (
