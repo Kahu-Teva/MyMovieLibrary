a4387ca08b50f58c1a3c3fb3429bc6ee0f67b442
Kahu-Teva <tevaa.kahu@gmail.com>
2021-04-11 20:58:19 -1000

    Components clean.

--- src/components/DirectorsList.js
+++ src/components/DirectorsList.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
-import "./../styles/PeopleList.css"
 import { Link } from "react-router-dom";
+import "./../styles/PeopleList.css"
 
 export default function DirectorList(){
   const [error, setError] = useState(null);
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
-import "./../styles/Header.css"
 import logo from "./../assets/logo.png"
+import "./../styles/Header.css"
 
 function Header(){
     return (
